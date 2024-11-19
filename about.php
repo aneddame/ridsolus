4de1a8cce9bcb7cc3ff150b2c8b4ff2f376454c5
ridwane aneddame <r.aneddame@gmail.com>
2024-11-19 17:55:23 +0100

    commit 1

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 
 <section id="about">
     <div class="container">
-        <h2>About Us</h2>
+        <h2>About Us :</h2>
         <p>Ridsolus is a tech-driven company offering top-notch solutions in web development, mobile apps, IoT, embedded systems, and social network management.</p>
     </div>
 </section>
--- includes/header.php
+++ includes/header.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ridsolus - Web & Mobile Development</title>
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <header>
     <div class="container">
 
-        <nav>
+        <nav class="divnav"> <ul>
+                <li><a href="index.php"><span class="bold-text">Ridsolus</span></a></li>
+
+            </ul>
             <ul>
-                <li><a href="index.php">Home</a></li>
+
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About</a></li>
                 <li><a href="services.php">Services</a></li>
